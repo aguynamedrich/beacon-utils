72da0329366ce703cd0629e1313e9523d4469212
aguynamedrich <aguynamedrich@gmail.com>
2012-08-24 11:21:40 -0400

    Added more overloads to UrlHelper.addParam.  addParam skips params if key or value is null or empty.  Added flag to turn off URL encoding when desired

--- Library/src/us/beacondigital/utils/UrlHelper.java
+++ Library/src/us/beacondigital/utils/UrlHelper.java
@@ -10,21 +10,47 @@ public class UrlHelper {
 	 * @param isFirst
 	 * @return
 	 */
-	public static String addParam(String url, String paramKey, String paramValue) {
-		boolean isFirst = url.indexOf("?") == -1;
-		String newUrl = String.format("%s%s%s=%s",
-				url,
-				((isFirst) ? "?" : "&"),
-				UrlParamEncoder.encode(paramKey),
-				UrlParamEncoder.encode(paramValue));
+	public static String addParam(String url, String paramKey, String paramValue, boolean encodingOn) {
+		String newUrl;
+		if(StringUtils.isNullOrEmpty(paramKey) || StringUtils.isNullOrEmpty(paramValue)) {
+			newUrl = url;
+		}
+		else {
+			boolean isFirst = url.indexOf("?") == -1;
+			String separator = isFirst ? "?" : "&";
+			String key = encodingOn ? UrlParamEncoder.encode(paramKey) : paramKey;
+			String value = encodingOn ? UrlParamEncoder.encode(paramValue) : paramValue;
+			
+			newUrl = String.format("%s%s%s=%s",
+					url,
+					separator,
+					key,
+					value);
+		}
 		return newUrl;
+	}
+	
+	public static String addParam(String url, String paramKey, String paramValue) {
+		return addParam(url, paramKey, paramValue, true);
 	}
 	
 	public static String addParam(String url, String paramKey, int paramValue) {
 		return addParam(url, paramKey, String.valueOf(paramValue));
 	}
 	
+	public static String addParam(String url, String paramKey, long paramValue) {
+		return addParam(url, paramKey, String.valueOf(paramValue));
+	}
+	
 	public static String addParam(String url, String paramKey, double paramValue) {
+		return addParam(url, paramKey, String.valueOf(paramValue));
+	}
+	
+	public static String addParam(String url, String paramKey, float paramValue) {
+		return addParam(url, paramKey, String.valueOf(paramValue));
+	}
+	
+	public static String addParam(String url, String paramKey, boolean paramValue) {
 		return addParam(url, paramKey, String.valueOf(paramValue));
 	}
 
--- Tests/src/us/beacondigital/samples/test/UrlHelperTests.java
+++ Tests/src/us/beacondigital/samples/test/UrlHelperTests.java
@@ -5,7 +5,7 @@ import android.test.AndroidTestCase;
 
 public class UrlHelperTests extends AndroidTestCase {
 	
-	public void testAppParamMethod() {
+	public void testAddParamMethod() {
 		
 		String baseUrl = "http://beacondigital.us";
 		String inputUrl, expected, actual;
@@ -24,6 +24,67 @@ public class UrlHelperTests extends AndroidTestCase {
 		expected = inputUrl + "?key%20with%20spaces=value";
 		actual = UrlHelper.addParam(inputUrl, "key with spaces", "value");
 		assertEquals("Params with special characters should be url encoded", expected, actual);
+		
+	}
+	
+	public void testAddParam_withEmptyStrings () {
+		
+		String baseUrl = "http://beacondigital.us";
+		String inputUrl, expected, actual;
+		
+		// Test empty value
+		inputUrl = baseUrl;
+		expected = inputUrl;
+		actual = UrlHelper.addParam(inputUrl, "key", "");
+		assertEquals("Passing an empty value should not add these params", expected, actual);
+		
+		// Test empty key
+		inputUrl = baseUrl;
+		expected = inputUrl;
+		actual = UrlHelper.addParam(inputUrl, "", "value");
+		assertEquals("Passing an empty key should not add these params", expected, actual);
+		
+		// Test null key
+		inputUrl = baseUrl;
+		expected = inputUrl;
+		actual = UrlHelper.addParam(inputUrl, null, "value");
+		assertEquals("Passing a null key should not add these params", expected, actual);
+		
+		// Test null value
+		inputUrl = baseUrl;
+		expected = inputUrl;
+		actual = UrlHelper.addParam(inputUrl, "key", null);
+		assertEquals("Passing a null value should not add these params", expected, actual);
+		
+	}
+	
+	public void testAddParam_encodingFlag () {
+		
+		String baseUrl = "http://beacondigital.us";
+		String inputUrl, expected, actual;
+		
+		// Test that url encoding is on by default
+		inputUrl = baseUrl;
+		
+		expected = inputUrl + "?key%20with%20spaces=value";
+		actual = UrlHelper.addParam(inputUrl, "key with spaces", "value");
+		assertEquals("Params key with special characters should be url encoded by default", expected, actual);
+		
+		expected = inputUrl + "?key=value%20with%20spaces";
+		actual = UrlHelper.addParam(inputUrl, "key", "value with spaces");
+		assertEquals("Params value with special characters should be url encoded by default", expected, actual);
+		
+		// Test that encoding can be turned off by flag
+		inputUrl = baseUrl;
+		
+		expected = inputUrl + "?key with spaces=value";
+		actual = UrlHelper.addParam(inputUrl, "key with spaces", "value", false);
+		assertEquals("Params key with special characters should not be url encoded when flag is false", expected, actual);
+		
+		expected = inputUrl + "?key=value with spaces";
+		actual = UrlHelper.addParam(inputUrl, "key", "value with spaces", false);
+		assertEquals("Params value with special characters should not be url encoded when flag is false", expected, actual);
+		
 		
 	}
 	
