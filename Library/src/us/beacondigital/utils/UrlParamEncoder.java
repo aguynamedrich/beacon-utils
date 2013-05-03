2912f3c9df3046f66214e1b95c40d574b99417f1
aguynamedrich <aguynamedrich@gmail.com>
2013-05-03 15:12:32 -0700

    Added ability to modify the set of unsafe characters in the UrlParamEncoder

--- Library/src/us/beacondigital/utils/UrlParamEncoder.java
+++ Library/src/us/beacondigital/utils/UrlParamEncoder.java
@@ -1,12 +1,18 @@
 package us.beacondigital.utils;
 
 public class UrlParamEncoder {
+	
+	public static final String DEFAULT_UNSAFE_CHARS = " %$&+,/:;=?@<>#%";
 
     public static String encode(String input) {
+    	return encode(input, DEFAULT_UNSAFE_CHARS);
+    }
+
+    public static String encode(String input, String chars) {
         StringBuilder encoded = new StringBuilder();
         for (char ch : input.toCharArray()) {
         	
-            if (isUnsafe(ch)) {
+            if (isUnsafe(ch, chars)) {
             	encoded.append('%');
             	encoded.append(toHex(ch / 16));
             	encoded.append(toHex(ch % 16));
@@ -15,18 +21,19 @@ public class UrlParamEncoder {
             }
         }
         return encoded.toString();
+    	
     }
 
     private static char toHex(int ch) {
         return (char) (ch < 10 ? '0' + ch : 'A' + ch - 10);
     }
 
-    private static boolean isUnsafe(char ch) {
+    private static boolean isUnsafe(char ch, String chars) {
     	boolean isUnsafe = false;
         if (ch > 128 || ch < 0) {
         	isUnsafe = true;
         }
-        else if (" %$&+,/:;=?@<>#%".indexOf(ch) >= 0) {
+        else if (chars.indexOf(ch) >= 0) {
         	isUnsafe = true;
         }
         return isUnsafe;
